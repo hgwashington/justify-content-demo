870858e3995f10f983503c7b6d5a38958663067d
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-07-12 12:01:16 -0400

    Working Flex-Start js

--- script/index.js
+++ script/index.js
@@ -1,1 +1,7 @@
 //js
+const flexStart = document.querySelector('[data-flex-start]');
+const flexStartDiv = document.querySelector('[data-flex-start-div]');
+
+flexStart.addEventListener('click', () => {
+    flexStartDiv.classList.toggle('hidden');
+});
